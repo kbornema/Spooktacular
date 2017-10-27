a080f9e883788d22e9a026394d47ecfa4da15a76
Roni <Roni@DESKTOP-ACT3AU0>
2017-10-27 22:18:51 +0200

    Structure for Linemover

---
+++ Assets/Scripts/Squad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squad : MonoBehaviour {
+
+    [SerializeField]
+    List<WayPoint> currentPath;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
