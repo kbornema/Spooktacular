979e60e723ab45bd0f73eb2fd31a1e7129aae3b7
Roni <Roni@DESKTOP-ACT3AU0>
2017-10-28 01:44:27 +0200

    Finish Interploation

--- Assets/Scripts/LineMover/WayPoint.cs
+++ Assets/Scripts/LineMover/WayPoint.cs
@@ -15,6 +15,8 @@ public class WayPoint : MonoBehaviour {
     WayPoint Left;
 
     [SerializeField]
+    GameObject TrackPrefab;
+
     GameObject track;
     [SerializeField]
     [Range(0.0f, 1.0f)]
@@ -22,21 +24,64 @@ public class WayPoint : MonoBehaviour {
     float oldPosition;
     [SerializeField]
     WayPoint activeWaypoint;
+    private int waypointScale = 1;
 
-	// Use this for initialization
-	void Start () {
-        //TODO interpolate between Position
+    // Use this for initialization
+    void Start () { 
+
         Vector3 origin = transform.position;
         Vector3 target = activeWaypoint.gameObject.transform.position;
         float distance = Vector3.Distance(origin, target);
         Debug.Log(distance+1);
 
+        //Create Track object
+        track = Instantiate(TrackPrefab, origin, Quaternion.identity, transform);
+        var trackObj = track.gameObject.transform;
+
+        //Rotate Track
+        Vector3 direction = target - origin;
+        float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        trackObj.transform.Rotate(Vector3.forward, rotation, Space.Self);
+
+        //Rescale Track
+        trackObj.transform.localScale = new Vector3(0, 0, 1);
+
+    }
+
+    public void updateWaypoint( DIRECTION newWaypoint)
+    {
+        switch( newWaypoint)
+        {
+            case DIRECTION.NORTH:
+                activeWaypoint = top;
+                break;
+            case DIRECTION.EAST:
+                activeWaypoint = right;
+                break;
+            case DIRECTION.SOUTH:
+                activeWaypoint = down;
+                break;
+            case DIRECTION.WEST:
+                activeWaypoint = Left;
+                break;
+        }
+    }
+
+    public float getCurrentInterpolation()
+    {
+        return positionInbetween;
+    }
+
+    public void setCurrentInterploation( float newInterpolation )
+    {
+ 
+        positionInbetween = Mathf.Clamp( newInterpolation, 0, 1);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(oldPosition != positionInbetween)
+        if(oldPosition != positionInbetween && activeWaypoint != null)
         {
             var targetObj = activeWaypoint.gameObject.transform;
             var trackObj = track.gameObject.transform;
@@ -45,19 +90,11 @@ public class WayPoint : MonoBehaviour {
             Vector3 origin = transform.position;
             Vector3 target = targetObj.position;
             float distance = Vector3.Distance(origin, target);
-            var scale = distance * positionInbetween + 1;
+            var scale = (distance + waypointScale *2) * positionInbetween; 
             Debug.Log(scale);
             trackObj.localScale = new Vector3( scale,1,1);
-            //targetObj.localPosition = new Vector3(scale, 0);//TODO both axis 
-
-
-            //Rotate
-            Vector3 direction = target - origin;
-            float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            transform.Rotate(Vector3.up, rotation, Space.Self);
-
-
+            //trackObj.localPosition = new Vector3(scale, 0);//TODO both axis 
+            
             //set for no new calculation
             oldPosition = positionInbetween;
         }
